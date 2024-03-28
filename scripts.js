1967c90b7bbf233d8e5396bc04f658b930a735c0
Mardavab <mardavab@gmail.com>
2024-03-27 21:12:19 -0500

    Se agrego el contador de tareas completadas

--- scripts.js
+++ scripts.js
@@ -1,5 +1,15 @@
 const inputBox = document.getElementById("input-box");
 const listContainer  = document.getElementById("list-container");
+let numPendiente = document.getElementById("num-Pendiente");
+let numTareasCom = 0;
+let numTareasPen = 0;
+let numTareas = localStorage.getItem("numTareas");
+
+if (numTareas === null) {
+    numTareas = 0;
+} else {
+    numTareas = parseInt(numTareas);
+}
 
 function addTask() {
     if (inputBox.value == '') {
@@ -7,14 +17,32 @@ function addTask() {
     }else{
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
-        listContainer .appendChild(li);
+        listContainer.appendChild(li);
         let span = document.createElement("span")
         span.innerHTML = '\u00d7';
-        li.appendChild(span)
+        li.appendChild(span);
+        numTareas = numTareas + 1;
+        numPendiente.innerHTML = numTareas;
+        
     }
     inputBox.value =  '';
     saveData();
 }
+
+listContainer.addEventListener("click", function(e) {
+    if (e.target.classList.contains("checked")) {
+        numTareasPen = numTareasPen + 1;
+        numTareas = numTareas + 1;
+        numPendiente.innerHTML = numTareas;
+
+        
+    }else{
+        numTareasCom = numTareasCom + 1;
+        numTareas = numTareas - 1;
+        numPendiente.innerHTML = numTareas;
+
+    }
+}, false);
 
 listContainer.addEventListener("click", function(e) {
     if (e.target.tagName === "LI") {
@@ -29,12 +57,17 @@ listContainer.addEventListener("click", function(e) {
 
 function saveData() {
     localStorage.setItem("data", listContainer.innerHTML);
+    localStorage.setItem("numTareas", numPendiente.innerHTML);
 }
 function showTask() {
     listContainer.innerHTML = localStorage.getItem("data");
+    numPendiente.innerHTML = localStorage.getItem("numTareas");
 }
 function clearData() {
     localStorage.removeItem("data");
-    listContainer.innerHTML = ""; // Clear the list container
+    localStorage.removeItem("numTareas");
+    listContainer.innerHTML = "";
+    numPendiente.innerHTML = "";
 }
+
 showTask();
